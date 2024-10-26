ed98aee20dc226c5123003f9d227483cdd10a7c2
InterPixelCoding <theweeklyjubs@gmail.com>
2024-10-26 19:39:02 +0100

    Build 1.1

--- script.js
+++ script.js
@@ -24,19 +24,28 @@ class answer_db {
 	get_hash(text, img, options) {
 		const hashed_text = String(CryptoJS.MD5(text));
 		let hash = hashed_text;
-		const img_hash = String(CryptoJS.MD5(img));
-		hash += img_hash;
 
 		const option_hash = String(CryptoJS.MD5(options.sort().join()));
 		hash += option_hash;
+		const img_hash = String(CryptoJS.MD5(img));
+		hash += img_hash;
+
 		return hash;
 	}
 
-	get_answer(text, img_url, options) {
-		const hash = this.get_hash(text, img_url, Array.from(options));
-		return this.hashed_db[
-			Object.keys(this.hashed_db).filter((x) => x === hash)
-		];
+	filter_keys(text, img_url, options) {
+		return Object.keys(this.hashed_db)
+			.filter((x) =>
+				text === "" ? true : x.includes(String(CryptoJS.MD5(text))),
+			)
+			.filter((x) =>
+				img_url === "" ? true : x.includes(String(CryptoJS.MD5(img_url))),
+			)
+			.filter((x) =>
+				options === ""
+					? true
+					: x.includes(String(CryptoJS.MD5(options.split(",").sort().join()))),
+			);
 	}
 }
 const answers = new answer_db();
@@ -47,20 +56,20 @@ window.onpageshow = () => on_load();
 async function get_answer(text, options, img) {
 	const answer_span = document.querySelector(".answer");
 	answer_span.style.opacity = "0";
-	const answer = answers.get_answer(
-		text,
-		img === "" ? undefined : img,
-		Array.from(options),
-	);
-
-	if (answer.length !== 0) {
-		answer_span.textContent = answer.join(", ");
+	console.log(text, options, img);
+	const answer = answers.filter_keys(text, img, options);
+	console.log(answer.length);
+	if (answer.length != 0) {
+		answer_span.innerHTML = `There are ${answer.length} matches: \
+        ${answer.map((x) => `"${answers.hashed_db[x].join(", ")}"`).join(", ")}`;
 	} else {
 		answer_span.textContent = "Answer not found";
-		console.log("Hash not found in database:", question_hash);
 	}
 
 	answer_span.style.opacity = "1";
+	document.querySelector("#question_text").value = "";
+	document.querySelector("#question_options").value = "";
+	document.querySelector("#question_img").value = "";
 }
 
 document.querySelector("button").onclick = () =>
